e5ecb97b2b3a116416345666c04ffaa12ee2dc71
Matt Shaw <matt@mttshw.com>
2025-12-12 15:51:57 +0000

    swatch picker WIP

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 export default (eleventyConfig) => {
   eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("swatch-picker");
   eleventyConfig.addPassthroughCopy("favicon.ico");
   eleventyConfig.addPassthroughCopy("favicon.png");
   return {
